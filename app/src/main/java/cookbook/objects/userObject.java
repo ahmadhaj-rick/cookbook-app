16e8e3859e04d590223f7525daef7a64360071fa
aa226un <aa226un@student.lnu.se>
2023-04-25 18:35:13 +0200

    renamed

--- app/src/main/java/cookbook/objects/userObject.java
+++ app/src/main/java/cookbook/objects/userObject.java
@@ -6,7 +6,7 @@ public class userObject {
   private String username;
   private String pass;
   private Boolean admin;
-  
+
   public userObject(int ID, String name, String username, String pass, Boolean isAdmin) {
     setUserId(ID);
     setName(name);
@@ -14,39 +14,39 @@ public class userObject {
     setPass(pass);
     setAdminPrivelages(isAdmin);
   }
-  
+
   public int getId() {
     return Id;
   }
-  
+
   private void setUserId(int inputId) {
     this.Id = inputId;
   }
-  
+
   public String getName() {
     return name;
   }
-  
+
   private void setName(String name) {
     this.name = name;
   }
-  
+
   public String getUser_name() {
     return username;
   }
-  
+
   private void setUserName(String user_name) {
     this.username = user_name;
   }
-  
+
   public String getPass() {
     return pass;
   }
-  
+
   private void setPass(String pass) {
     this.pass = pass;
   }
-  
+
   private void setAdminPrivelages(Boolean isAdmin) {
     if(isAdmin == true) {
       this.admin = isAdmin;
@@ -54,12 +54,12 @@ public class userObject {
       this.admin = null;
     }
   }
-  
+
   public Boolean getAdminPrivelages(){
     return admin;
   }
+
   
-  
-  
+
   
 }
