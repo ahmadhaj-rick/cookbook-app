f2b4d90a54d3c87631c1f1d7469beb29cfe59cc1
Abdul Rahman Racheed <ar224ee@student.lnu.se>
2023-04-26 17:26:40 +0200

    Logic for user add finished

---
+++ app/src/main/java/cookbook/javaFX/mainMenu.java
@@ -0,0 +1,39 @@
+package cookbook.javaFX;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import cookbook.objectControllers.userController;
+import cookbook.objects.userObject;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class mainMenu {
+    
+    @FXML 
+    public Button homebtn;
+    @FXML 
+    public Button addbuttom;
+    @FXML 
+    public Button inboxbutton;
+    @FXML 
+    public Button weeklistbutton;
+    @FXML 
+    public Button helpbutton;
+    @FXML 
+    public Button adminPanel;
+    @FXML 
+    public Button quitbutton;
+
+ /*   public void homePage(AtionEvent event) throws SQLException, IOException {
+
+    }*/
+
+}
--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module cookbook {
-  requires javafx.controls;
-  requires javafx.fxml;
-  requires java.sql;
-  requires javafx.graphics;
+  requires transitive javafx.controls;
+  requires transitive javafx.fxml;
+  requires transitive java.sql;
+  requires transitive javafx.graphics;
+  requires transitive javafx.base;
 
   exports cookbook;
-  opens cookbook to javafx.graphics, javafx.fxml;
+  opens cookbook to javafx.graphics;
+  exports cookbook.javaFX to javafx.fxml;
 }
