5aba605c191da2387de91376c7cc0c0d869e9b13
aa226un <aa226un@student.lnu.se>
2023-04-27 07:24:20 +0200

    filled the TableView colums with user info

--- app/src/main/java/cookbook/javaFX/adminPanel.java
+++ app/src/main/java/cookbook/javaFX/adminPanel.java
@@ -4,20 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import cookbook.objectControllers.userController;
 import cookbook.objects.userObject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 
-public class adminPanel {
+public class adminPanel implements Initializable {
   @FXML 
   public Button modifyuser;
   @FXML 
@@ -33,6 +40,7 @@ public class adminPanel {
   @FXML
   public TableView<userObject> userlst;
 
+  public List<userObject> users;
 
   public void adminCreateUser(ActionEvent event) throws SQLException, IOException {
     String name = txtDisplayName.getText();
@@ -53,6 +61,37 @@ public class adminPanel {
     
 
     
+  }
+
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    try {
+      users = userController.getUsers();
+  } catch (SQLException e) {
+      e.printStackTrace();
+  }
+  System.out.println(users.size());
+  ObservableList<userObject> userList = FXCollections.observableArrayList(users);
+
+  userlst.getColumns().clear();
+  // for some reasons there is an empty colum !!!!
+
+  TableColumn<userObject, String> nameColumn = new TableColumn<>("Name");
+  nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+  TableColumn<userObject, String> usernameColumn = new TableColumn<>("User Name");
+  usernameColumn.setCellValueFactory(new PropertyValueFactory<>("user_name"));
+
+  TableColumn<userObject, String> passwordColumn = new TableColumn<>("Password");
+  passwordColumn.setCellValueFactory(new PropertyValueFactory<>("pass"));
+  
+  userlst.getColumns().add(nameColumn);
+  userlst.getColumns().add(usernameColumn);
+  userlst.getColumns().add(passwordColumn);
+  userlst.getItems().clear();
+  userlst.setItems(userList);
+
   }
 
 
