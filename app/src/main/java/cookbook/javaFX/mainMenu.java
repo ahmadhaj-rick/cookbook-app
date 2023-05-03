3dd4afb101cf3343177441e99266944f467c672b
aa226un <aa226un@student.lnu.se>
2023-05-03 19:19:09 +0200

    fixed onAction and controller attributes

--- app/src/main/java/cookbook/javaFX/mainMenu.java
+++ app/src/main/java/cookbook/javaFX/mainMenu.java
@@ -14,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -33,7 +34,15 @@ public class mainMenu {
   public Button adminPanel;
   @FXML 
   public Button quitbutton;
+  @FXML
+  public Label UserNameMain;
   
+  
+  
+  public void setUserName(ActionEvent event) throws IOException {
+    userObject loggedUser = userController.loggedInUser;
+    UserNameMain.setText("Welcome: " + loggedUser.getName());
+  }
 
   public void homeClick(ActionEvent event) throws SQLException, IOException {
     
