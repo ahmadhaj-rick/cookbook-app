02fa4c64e3f25a89ea44cc9e0af79dae2b5c49e8
aa226un <aa226un@student.lnu.se>
2023-05-06 19:14:56 +0200

    small fix

--- app/src/main/java/cookbook/javaFX/homePage.java
+++ app/src/main/java/cookbook/javaFX/homePage.java
@@ -25,16 +25,16 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class homePage implements Initializable {
-
+  
   @FXML
   public TextField search;
   @FXML
   public Text IngField;
   @FXML
   public TableView<recipeObject> recipeLists;
-
+  
   public List<recipeObject> recipes;
-
+  
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     try {
@@ -44,16 +44,16 @@ public class homePage implements Initializable {
     }
     System.out.println(recipes.size() + "Size of elements");
     ObservableList<recipeObject> recipeList = FXCollections.observableArrayList(recipes);
-
+    
     recipeLists.getColumns().clear();
-
+    
     TableColumn<recipeObject, String> recipeNameColumn = new TableColumn<>("Name");
     recipeNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
+    
     recipeLists.getColumns().add(recipeNameColumn);
     recipeLists.getItems().clear();
     recipeLists.setItems(recipeList);
-
+    
     recipeLists.setOnMouseClicked(new EventHandler<MouseEvent>() {
       @Override
       public void handle(MouseEvent event) {
@@ -79,33 +79,53 @@ public class homePage implements Initializable {
         }
       }
     });
-
+    
   }
-
+  
   public void searchMethod() throws SQLException, IOException {
     String searchTxt = search.getText();
     recipeControler controller = new recipeControler(); // create an instance of recipeControler
-    List<recipeObject> recipes = controller.getRecpies(); // call the getRecpies()
-    List<recipeObject> filteredRecipes = new ArrayList<>();
+    // Saving Arrays
+    List<recipeObject> recipes = controller.getRecpies(); // Store recipes
+    List<recipeObject> filteredRecipes = new ArrayList<>(); // filtered recipes
     
-    String[] ingredientsToSearch = searchTxt.split(","); // assuming ingredients are separated by commas
+    
+    String[] searchWord =searchTxt.split(",");
     
     for (recipeObject recipe : recipes) {
       List<ingredientObject> ingredients = recipe.getIngredientsList();
-      for (String ingToSearch : ingredientsToSearch) {
+      List<tagObject> tags = recipe.getTagList();
+      boolean tagMatch = false;
+      boolean ingMatch = false;
+      
+      for (String word : searchWord) {
+        word = word.trim().toLowerCase();
+        
         for (ingredientObject ing : ingredients) {
           // check if the ingredient name contains the search string
-          if (ing.getName().toLowerCase().contains(ingToSearch.trim().toLowerCase())){
-            if (!filteredRecipes.contains(recipe)) {
-              // add the recipe to the filtered list if it hasn't been added already
-              filteredRecipes.add(recipe);
-            }
+          if (ing.getName().toLowerCase().contains(word)){
+            ingMatch = true;
+            break;
           }
         }
+
+        for (tagObject tag : tags) {
+          // check if the recipe has the tag.
+          if (tag.getTag_name().toLowerCase().contains(word)) {
+            tagMatch = true;
+            break;
+          }
+        }
+
+      }
+      if (tagMatch || ingMatch) {
+        filteredRecipes.add(recipe);
       }
     }
     // set the items of the TableView to the filtered list of recipes
     ObservableList<recipeObject> observableFilteredRecipes = FXCollections.observableArrayList(filteredRecipes);
     recipeLists.setItems(observableFilteredRecipes);
   }
+
 }
+
