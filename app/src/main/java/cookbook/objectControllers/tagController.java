5778221f7fe93376e0ea87cff8665f499a93a05b
Iker Garcia Martinez <ig222kd@student.lnu.se>
2023-05-15 17:29:02 +0200

    add tag to recipe hotfix baby

--- app/src/main/java/cookbook/objectControllers/tagController.java
+++ app/src/main/java/cookbook/objectControllers/tagController.java
@@ -56,5 +56,18 @@ public class tagController {
         
     
     }
+
+    public static void addTagToRecipe(String RecipeID, String TagID) throws SQLException {
+      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+      String query = "INSERT INTO recipe_tag VALEUS(?,?)";
+      try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+        sqlStatement.setString(1, RecipeID);
+        sqlStatement.setString(2, TagID);
+        sqlStatement.executeUpdate();
+
+        int rowsAffected = sqlStatement.executeUpdate();
+        System.out.println(rowsAffected + "rows affected");
+      }
+    }
 }
 
