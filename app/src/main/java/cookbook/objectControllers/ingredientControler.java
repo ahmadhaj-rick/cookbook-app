923c5a135ddfec210962bae7e27dccd996abd000
Iker Garcia Martinez <ig222kd@student.lnu.se>
2023-05-15 16:39:32 +0200

    Added a method that introduced a way to add values into recipe_ingredients table in SQL

--- app/src/main/java/cookbook/objectControllers/ingredientControler.java
+++ app/src/main/java/cookbook/objectControllers/ingredientControler.java
@@ -12,7 +12,7 @@ import java.sql.ResultSet;
 import cookbook.objects.ingredientObject;
 
 public class ingredientControler {
-    
+  
   public static List<ingredientObject> getIngredients() throws SQLException {
     ArrayList<ingredientObject> currentIngredients = new ArrayList<>();
     String query = "SELECT * FROM ingredients";
@@ -21,29 +21,46 @@ public class ingredientControler {
       ResultSet result = sqlstatement.executeQuery();
       while(result.next()) {
         ingredientObject newIngredient = new ingredientObject(
-          result.getString("ingredient_id"),
-          result.getString("ingredient_name"));
+        result.getString("ingredient_id"),
+        result.getString("ingredient_name"));
         currentIngredients.add(newIngredient);
       }
       result.close();
     }
     catch (SQLException e) {
       System.out.println(e);
-  } 
-  return currentIngredients;
-}
+    } 
+    return currentIngredients;
+  }
+  
+  
+  public static void addIngredient (String uniqueID, String name) throws SQLException{
+    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+    String query = "INSERT INTO ingredients VALUES(?,?)";
+    try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+      sqlStatement.setString(1, uniqueID);
+      sqlStatement.setString(2, name);
+      sqlStatement.executeUpdate();
+    }
+    catch (SQLException e) {
+      System.out.println(e);
+    }
+  }
+ 
+  public static void addIngredientToRecipe(String recipeID, String ingredientID) throws SQLException {
+    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+    String query = "INSERT INTO recipe_ingredients VALUES (?,?)";
+    try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+      sqlStatement.setString(1, recipeID);
+      sqlStatement.setString(2, ingredientID);
+      sqlStatement.execute();
+      
+      int rowsAffected = sqlStatement.executeUpdate();
+      System.out.println(rowsAffected + " rows affected");
 
+    } catch (SQLException e) {
+      System.out.println(e);
+    }
+  }
 
-public static void addIngredient (String uniqueID, String name) throws SQLException{
-Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
-String query = "INSERT INTO ingredients VALUES(?,?)";
-try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
-  sqlStatement.setString(1, uniqueID);
-  sqlStatement.setString(2, name);
-  sqlStatement.executeUpdate();
-}
-catch (SQLException e) {
-  System.out.println(e);
-}
-}
 }
