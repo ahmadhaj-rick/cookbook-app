183332029bfb94695313b7495d8c2d2be920bfb2
Tsnem Musa <tm222qf@student.lnu.se>
2023-05-20 10:45:18 +0200

    create messageobject class

---
+++ app/src/main/java/cookbook/objects/MessageObject.java
@@ -0,0 +1,13 @@
+package cookbook.objects;
+
+import java.sql.Timestamp;
+
+public class MessageObject {
+  private String id;
+  private String fromUserId;
+  private String toUserId;
+  private String recipe_Id;
+  private String body;
+  private Timestamp createdAt;
+
+}
