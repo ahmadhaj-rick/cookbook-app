bfc85bcdc5d854c1247d3cf97465da1dd536ba38
Tsnem Musa <tm222qf@student.lnu.se>
2023-05-20 13:53:43 +0200

    Generate a unique ID for the message

--- app/src/main/java/cookbook/objectControllers/MessageController.java
+++ app/src/main/java/cookbook/objectControllers/MessageController.java
@@ -2,6 +2,7 @@ package cookbook.objectControllers;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class MessageController {
 
@@ -9,6 +10,11 @@ public class MessageController {
 
   public static void sendMessage(String id, String from_User, String to_User, String recipe_Id, String body,
   Timestamp created_at){
+
+
+    // Generate a unique ID for the message.
+    UUID uniqueID = UUID.randomUUID();
+    String messageID = uniqueID.toString();
     
   }
 
