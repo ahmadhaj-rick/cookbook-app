90758dead9b560f7582016bc21f18c447c18881c
Tsnem Musa <tm222qf@student.lnu.se>
2023-05-20 13:45:21 +0200

    create MessageController file

---
+++ app/src/main/java/cookbook/objectControllers/MessageController.java
@@ -0,0 +1,17 @@
+package cookbook.objectControllers;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class MessageController {
+
+  // send a message from one user to another.
+
+  public static void sendMessage(String id, String from_User, String to_User, String recipe_Id, String body,
+  Timestamp created_at){
+    
+  }
+
+
+
+}
