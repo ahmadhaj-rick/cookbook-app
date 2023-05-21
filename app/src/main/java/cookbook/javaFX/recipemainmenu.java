9620f87d6350b4b0af4e028131fd991fa9c3b203
aa226un <aa226un@student.lnu.se>
2023-05-21 20:24:54 +0200

    addRecipe works now

--- app/src/main/java/cookbook/javaFX/recipemainmenu.java
+++ app/src/main/java/cookbook/javaFX/recipemainmenu.java
@@ -47,9 +47,6 @@ public class recipemainmenu implements Initializable{
   public ComboBox<String> tagsDropdown;
   
   @FXML
-  public ComboBox<recipeObject> categoryBox;
-  
-  @FXML
   public TextField tagName;
   
   @FXML
@@ -57,9 +54,6 @@ public class recipemainmenu implements Initializable{
   
   @FXML
   public TextField ingredientName;
-  
-  @FXML
-  public TextField categoryName;
   
   @FXML
   public Button addIngredient;
