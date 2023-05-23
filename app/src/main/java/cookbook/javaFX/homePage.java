7e43c0c6292cbd666a4ec5b2e9ea73fd2e78a64c
aa226un <aa226un@student.lnu.se>
2023-05-23 13:49:30 +0200

    app runns now after merge

--- app/src/main/java/cookbook/javaFX/homePage.java
+++ app/src/main/java/cookbook/javaFX/homePage.java
@@ -53,8 +53,7 @@ public class homePage implements Initializable {
   public Button back;
   @FXML
   public Label recipeName;
-  @FXML
-  public Button addToFavorite;
+  
   
   public List<recipeObject> recipes;
   
@@ -70,16 +69,16 @@ public class homePage implements Initializable {
     }
     System.out.println(recipes.size() + "Size of elements");
     ObservableList<recipeObject> recipeList = FXCollections.observableArrayList(recipes);
-
+    
     recipeLists.getColumns().clear();
-
+    
     TableColumn<recipeObject, String> recipeNameColumn = new TableColumn<>("Name");
     recipeNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
+    
     recipeLists.getColumns().add(recipeNameColumn);
     recipeLists.getItems().clear();
     recipeLists.setItems(recipeList);
-
+    
     recipeLists.setOnMouseClicked(new EventHandler<MouseEvent>() {
       @Override
       public void handle(MouseEvent event) {
@@ -106,20 +105,13 @@ public class homePage implements Initializable {
               sb2.append(tag.getTag_name()).append(", ");
             }
             tagField.setText(sb2.toString());
-
+            
             IngField.setText(sb.toString());
           }
         }
-        for (tagObject tag : tagObjects) {
-          sb2.append(tag.getTag_name()).append(", ");
-        }
-        tagField.setText(sb2.toString());
-        
-        IngField.setText(sb.toString());
-        
       }
     });
-
+    
     // Add a listener to the TableView selection model
     recipeLists.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<recipeObject>() {
       @Override
@@ -128,151 +120,150 @@ public class homePage implements Initializable {
         updateIngredientsText(); // Update the displayed ingredients
       }
     });
-  }
-}); */
-
-}
-
-public void searchMethod() throws SQLException, IOException {
-  String searchTxt = search.getText();
-  recipeControler controller = new recipeControler(); // create an instance of recipeControler
-  // Saving Arrays
-  List<recipeObject> recipes = controller.getRecpies(); // Store recipes
-  List<recipeObject> filteredRecipes = new ArrayList<>(); // filtered recipes
-  
-  
-  String[] searchWord =searchTxt.split(",");
-  
-  for (recipeObject recipe : recipes) {
-    List<QuanitityIngredients> ingredients = recipe.getIngredientsList();
-    List<tagObject> tags = recipe.getTagList();
-    boolean tagMatch = false;
-    boolean ingMatch = false;
     
-    for (String word : searchWord) {
-      word = word.trim().toLowerCase();
+    
+  }
+  
+  
+  public void searchMethod() throws SQLException, IOException {
+    String searchTxt = search.getText();
+    recipeControler controller = new recipeControler(); // create an instance of recipeControler
+    // Saving Arrays
+    List<recipeObject> recipes = controller.getRecpies(); // Store recipes
+    List<recipeObject> filteredRecipes = new ArrayList<>(); // filtered recipes
+    
+    
+    String[] searchWord =searchTxt.split(",");
+    
+    for (recipeObject recipe : recipes) {
+      List<QuanitityIngredients> ingredients = recipe.getIngredientsList();
+      List<tagObject> tags = recipe.getTagList();
+      boolean tagMatch = false;
+      boolean ingMatch = false;
       
-      for (QuanitityIngredients ing : ingredients) {
-        // check if the ingredient name contains the search string
-        if (ing.getName().toLowerCase().contains(word)){
-          ingMatch = true;
-          break;
+      for (String word : searchWord) {
+        word = word.trim().toLowerCase();
+        
+        for (QuanitityIngredients ing : ingredients) {
+          // check if the ingredient name contains the search string
+          if (ing.getName().toLowerCase().contains(word)){
+            ingMatch = true;
+            break;
+          }
         }
+        
+        for (tagObject tag : tags) {
+          // check if the recipe has the tag.
+          if (tag.getTag_name().toLowerCase().contains(word)) {
+            tagMatch = true;
+            break;
+          }
+        }
+        
+      }
+      if (tagMatch || ingMatch) {
+        filteredRecipes.add(recipe);
+      }
+    }
+    // set the items of the TableView to the filtered list of recipes
+    ObservableList<recipeObject> observableFilteredRecipes = FXCollections.observableArrayList(filteredRecipes);
+    recipeLists.setItems(observableFilteredRecipes);
+  }
+  
+  public void updateFavorite() throws SQLException {
+    try {
+      recipeControler recipeController = new recipeControler();
+      recipeObject selectedRecipe = recipeLists.getSelectionModel().getSelectedItem();
+      recipeController.updateFavoriteStatus(selectedRecipe);
+      System.out.println(selectedRecipe.getStar());
+      
+      if (selectedRecipe.getStar() == true) {
+        Alert addedToFav = new Alert(AlertType.CONFIRMATION);
+        addedToFav.setTitle("Added to Fav!!");
+        addedToFav.setContentText("You have added this recipe to Favorite!");
+        addedToFav.show();
+      } else {
+        Alert addedToFav = new Alert(AlertType.CONFIRMATION);
+        addedToFav.setTitle("removed from Fav!!");
+        addedToFav.setContentText("You have removed this recipe from Favorite!");
+        addedToFav.show();
       }
       
-      for (tagObject tag : tags) {
-        // check if the recipe has the tag.
-        if (tag.getTag_name().toLowerCase().contains(word)) {
-          tagMatch = true;
-          break;
-        }
-      }
-      
-    }
-    if (tagMatch || ingMatch) {
-      filteredRecipes.add(recipe);
-    }
-  }
-  // set the items of the TableView to the filtered list of recipes
-  ObservableList<recipeObject> observableFilteredRecipes = FXCollections.observableArrayList(filteredRecipes);
-  recipeLists.setItems(observableFilteredRecipes);
-}
-
-
-public void updateFavorite() throws SQLException {
-  try {
-    recipeControler recipeController = new recipeControler();
-    recipeObject selectedRecipe = recipeLists.getSelectionModel().getSelectedItem();
-    recipeController.updateFavoriteStatus(selectedRecipe);
-    System.out.println(selectedRecipe.getStar());
-    
-    if (selectedRecipe.getStar() == true) {
-      Alert addedToFav = new Alert(AlertType.CONFIRMATION);
-      addedToFav.setTitle("Added to Fav!!");
-      addedToFav.setContentText("You have added this recipe to Favorite!");
-      addedToFav.show();
-    } else {
-      Alert addedToFav = new Alert(AlertType.CONFIRMATION);
-      addedToFav.setTitle("removed from Fav!!");
-      addedToFav.setContentText("You have removed this recipe from Favorite!");
-      addedToFav.show();
+    } catch (Exception e) {
+      System.out.println(e + "fav method problem");
     }
     
-  } catch (Exception e) {
-    System.out.println(e + "fav method problem");
+    
   }
   
   
-}
-
-
-/*public void favoriteRecipeList() throws SQLException {
-  List<recipeObject> faveList = recipeControler.favoriteObjects();
-  ObservableList<recipeObject> observableFavList = FXCollections.observableArrayList(faveList);
-  recipeLists.setItems(observableFavList);
-  
-}*/
-
-public void getFilteredRecipes(ActionEvent event) throws SQLException {
-  CheckBox favoritecheck = (CheckBox) event.getSource();
-  if (favoritecheck.isSelected()) {
+  /*public void favoriteRecipeList() throws SQLException {
     List<recipeObject> faveList = recipeControler.favoriteObjects();
     ObservableList<recipeObject> observableFavList = FXCollections.observableArrayList(faveList);
     recipeLists.setItems(observableFavList);
-  } else {
-    List<recipeObject> normalList = recipeControler.getRecpies();
-    ObservableList<recipeObject> observablenormList = FXCollections.observableArrayList(normalList);
-    recipeLists.setItems(observablenormList);
+    
+  }*/
+  
+  public void getFilteredRecipes(ActionEvent event) throws SQLException {
+    CheckBox favoritecheck = (CheckBox) event.getSource();
+    if (favoritecheck.isSelected()) {
+      List<recipeObject> faveList = recipeControler.favoriteObjects();
+      ObservableList<recipeObject> observableFavList = FXCollections.observableArrayList(faveList);
+      recipeLists.setItems(observableFavList);
+    } else {
+      List<recipeObject> normalList = recipeControler.getRecpies();
+      ObservableList<recipeObject> observablenormList = FXCollections.observableArrayList(normalList);
+      recipeLists.setItems(observablenormList);
+    }
   }
-}
-
-// adjust the number of persons a recipe is
-private void updateIngredientsText() {
-  StringBuilder ingredientsString = new StringBuilder();
-  recipeObject recp = recipeLists.getSelectionModel().getSelectedItem();
-  for (QuanitityIngredients quantifiedIngredient : recp.getIngredientsList()) {
-    float ingredientAmount = quantifiedIngredient.getAmount() * portions;
-    String ingredientUnit = quantifiedIngredient.getUnit() != null ? " " + quantifiedIngredient.getUnit() : "";
-    String ingredientName = quantifiedIngredient.getIngredient().getName();
-    ingredientsString.append(String.format("%s%s %s\n", ingredientAmount, ingredientUnit, ingredientName));
+  
+  // adjust the number of persons a recipe is
+  private void updateIngredientsText() {
+    StringBuilder ingredientsString = new StringBuilder();
+    recipeObject recp = recipeLists.getSelectionModel().getSelectedItem();
+    for (QuanitityIngredients quantifiedIngredient : recp.getIngredientsList()) {
+      float ingredientAmount = quantifiedIngredient.getAmount() * portions;
+      String ingredientUnit = quantifiedIngredient.getUnit() != null ? " " + quantifiedIngredient.getUnit() : "";
+      String ingredientName = quantifiedIngredient.getIngredient().getName();
+      ingredientsString.append(String.format("%s%s %s\n", ingredientAmount, ingredientUnit, ingredientName));
+    }
+    IngField.setText(ingredientsString.toString());
+    portionsLabel.setText(String.valueOf(portions));
   }
-  IngField.setText(ingredientsString.toString());
-  portionsLabel.setText(String.valueOf(portions));
-}
-
-@FXML
-void onDecreasePortions(ActionEvent event) {
-  if (portions > 1) {
-    portions--;
+  
+  @FXML
+  void onDecreasePortions(ActionEvent event) {
+    if (portions > 1) {
+      portions--;
+      updateIngredientsText();
+    }
+  }
+  
+  @FXML
+  void onIncreasePortions(ActionEvent event) {
+    portions++;
     updateIngredientsText();
   }
-}
-
-@FXML
-void onIncreasePortions(ActionEvent event) {
-  portions++;
-  updateIngredientsText();
-}
-
-public void backButton(ActionEvent event) throws SQLException, IOException {
-  URL url = new File("src/main/java/cookbook/resources/mainmenu.fxml").toURI().toURL();
-  FXMLLoader loader = new FXMLLoader(url);
-  Parent root = loader.load();
-  Scene loginScene = new Scene(root);
   
-  Stage mainStage = (Stage) back.getScene().getWindow();
-  mainStage.setScene(loginScene);
-  mainStage.show();
-  mainStage.setHeight(740);
-  mainStage.setWidth(1000);
-  mainStage.setResizable(true);
-  mainStage.centerOnScreen();
-  userController user = new userController();
-  String name = user.loggedInUser.getName();
-  mainStage.setTitle("Welcome back to the main menu dear " + name );
+  public void backButton(ActionEvent event) throws SQLException, IOException {
+    URL url = new File("src/main/java/cookbook/resources/mainmenu.fxml").toURI().toURL();
+    FXMLLoader loader = new FXMLLoader(url);
+    Parent root = loader.load();
+    Scene loginScene = new Scene(root);
+    
+    Stage mainStage = (Stage) back.getScene().getWindow();
+    mainStage.setScene(loginScene);
+    mainStage.show();
+    mainStage.setHeight(740);
+    mainStage.setWidth(1000);
+    mainStage.setResizable(true);
+    mainStage.centerOnScreen();
+    userController user = new userController();
+    String name = user.loggedInUser.getName();
+    mainStage.setTitle("Welcome back to the main menu dear " + name );
+    
+  }
   
-}
-
-
+  
 }
 
