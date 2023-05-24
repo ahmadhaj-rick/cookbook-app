d69f8820b33794710697199692322f5485fb6515
Abdul Rahman Racheed <ar224ee@student.lnu.se>
2023-05-24 23:48:06 +0200

    Added a CommentObject

---
+++ app/src/main/java/cookbook/objects/CommentObject.java
@@ -0,0 +1,52 @@
+package cookbook.objects;
+
+public class CommentObject {
+  private String id;
+  private String text;
+  private String userID;
+  private String recipeID;
+  
+
+  public CommentObject(String id, String text, String userID, String recipeID) {
+    setId(id);
+    setText(text);
+    setUserId(userID);
+    setRecipeId(recipeID);
+  }
+
+
+  private void setId(String ID) {
+    this.id = ID;
+  }
+
+  public String getID() {
+    return id;
+  }
+
+  private void setText(String text) {
+    this.text = text;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  private void setUserId(String userID) {
+    this.userID = userID;
+  }
+
+  public String getUserId() {
+    return userID;
+  }
+
+  private void setRecipeId(String recipeID) {
+    this.recipeID = recipeID;
+  }
+
+  public String getRecipeID() {
+    return recipeID;
+  }
+
+
+
+}
