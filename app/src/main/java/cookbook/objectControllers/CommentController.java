c22f91465771fefe5aac51babfae4cb85dea157b
Abdul Rahman Racheed <ar224ee@student.lnu.se>
2023-05-24 23:59:24 +0200

    Added the editComment method for future usage aswell as reindented so it looks cleaner.

--- app/src/main/java/cookbook/objectControllers/CommentController.java
+++ app/src/main/java/cookbook/objectControllers/CommentController.java
@@ -6,32 +6,43 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CommentController {
-    
-    public static void addComment(String ID, String text, String userID, String recipeID) throws SQLException {
-        String query = "INSERT INTO comment VALUES (?,?,?,?)";
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
-        try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
-          sqlStatement.setString(1, ID);
-          sqlStatement.setString(2, text);
-          sqlStatement.setString(3, userID);
-          sqlStatement.setString(4, recipeID);
-          sqlStatement.executeUpdate();
-        } catch (SQLException e) {
-          System.out.println("Error adding comment," + e);
-        }
-      }
-    
-
-      public void deleteComment(String id) throws SQLException{
-        String query = "DELETE FROM comment WHERE id=(?)";
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
-        try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
-          sqlStatement.setString(1, id);
-          sqlStatement.executeUpdate();
-        } catch (SQLException e) {
-          System.out.println("Error deleting comment." + e);
-        }
-    
-      }
-    
+  
+  public static void addComment(String ID, String text, String userID, String recipeID) throws SQLException {
+    String query = "INSERT INTO comment VALUES (?,?,?,?)";
+    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+    try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+      sqlStatement.setString(1, ID);
+      sqlStatement.setString(2, text);
+      sqlStatement.setString(3, userID);
+      sqlStatement.setString(4, recipeID);
+      sqlStatement.executeUpdate();
+    } catch (SQLException e) {
+      System.out.println("Error adding comment," + e);
+    }
+  }
+  
+  
+  public void deleteComment(String id) throws SQLException{
+    String query = "DELETE FROM comment WHERE id=(?)";
+    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+    try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+      sqlStatement.setString(1, id);
+      sqlStatement.executeUpdate();
+    } catch (SQLException e) {
+      System.out.println("Error deleting comment." + e);
+    }
+  }
+  
+  public void editComment(String id, String text) throws SQLException {
+    String query = "UPDATE comment SET text=(?) WHERE id=(?)";
+    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+    try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+      sqlStatement.setString(1, text);
+      sqlStatement.setString(2, id);
+      sqlStatement.executeUpdate();
+    } catch (SQLException e) {
+      System.out.println("Error editing comment," + e);
+    }
+  }
+  
 }
