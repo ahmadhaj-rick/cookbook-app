1386328339e396fbc6f2b5abc4b595c24bba7c3d
Abdul Rahman Racheed <ar224ee@student.lnu.se>
2023-05-24 23:56:12 +0200

    Created the comment Controller aswell as introduced the first add Query

---
+++ app/src/main/java/cookbook/objectControllers/CommentController.java
@@ -0,0 +1,25 @@
+package cookbook.objectControllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CommentController {
+    
+    public static void addComment(String ID, String text, String userID, String recipeID) throws SQLException {
+        String query = "INSERT INTO comment VALUES (?,?,?,?)";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cookbook?user=root&password=root&useSSL=false");
+        try (PreparedStatement sqlStatement = conn.prepareStatement(query)) {
+          sqlStatement.setString(1, ID);
+          sqlStatement.setString(2, text);
+          sqlStatement.setString(3, userID);
+          sqlStatement.setString(4, recipeID);
+          sqlStatement.executeUpdate();
+        } catch (SQLException e) {
+          System.out.println("Error adding comment," + e);
+        }
+      }
+    
+
+}
