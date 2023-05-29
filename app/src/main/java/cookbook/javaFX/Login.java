d544e217f6cea14309a44ce8235a09163ebf9643
Kumail Janjua <kj222yh@student.lnu.se>
2023-05-29 15:46:52 +0200

    working on login.java,addingcomments

--- app/src/main/java/cookbook/javaFX/Login.java
+++ app/src/main/java/cookbook/javaFX/Login.java
@@ -27,6 +27,19 @@ public class Login implements Initializable {
   @FXML
   public Button loginButton;
 
+/**
+ * Handles the action when the user login button is clicked.
+ * Retrieves the entered username and password from the user interface.
+ * Searches for a user with the given username and password using the userController's searchForUser() method.
+ * If a user is found, it loads the mainmenu.fxml file, sets it as the root of the scene, and displays the scene in the main menu stage.
+ * Sets the stage dimensions, centers it on the screen, and disables resizing.
+ * Displays an error message if the login credentials are invalid.
+ *
+ * @param event The action event that triggered the method.
+ * @throws SQLException If there is an SQL-related error.
+ * @throws IOException  If there is an I/O error.
+ */
+
   public void userLogin(ActionEvent event) throws SQLException, IOException {
     String username = user.getText();
     String pass = passwordtxt.getText();
@@ -70,11 +83,27 @@ public class Login implements Initializable {
     }
   }
 
+/**
+ * Initializes the Login controller after its root element has been completely processed.
+ * Calls the toggleShowPassword() method with a null parameter.
+ *
+ * @param location  The location used to resolve relative paths for the root object, or null if the location is not known.
+ * @param resources The resources used to localize the root object, or null if the root object was not localized.
+ */
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     this.toggleShowPassword(null);
 
   }
+
+/**
+ * Toggles the visibility of the password fields based on the state of the show password checkbox.
+ * If the checkbox is selected, it sets the text of the passtext field to match the passwordtxt field and shows the passtext field while hiding the passwordtxt field.
+ * If the checkbox is not selected, it sets the text of the passwordtxt field to match the passtext field and shows the passwordtxt field while hiding the passtext field.
+ *
+ * @param actionEvent The action event that triggered the method.
+ */
 
   public void toggleShowPassword(ActionEvent actionEvent) {
       if (showPass.isSelected()) {
