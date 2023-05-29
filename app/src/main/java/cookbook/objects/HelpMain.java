e2d485aadafac4ffb01aa0d047b7db21b713b068
Kumail Janjua <kj222yh@student.lnu.se>
2023-05-29 17:00:57 +0200

    reviewed and commented helpmain file

--- app/src/main/java/cookbook/objects/HelpMain.java
+++ app/src/main/java/cookbook/objects/HelpMain.java
@@ -1,17 +1,40 @@
 package cookbook.objects;
 
+/**
+ * The HelpMain class represents a help item with its title and body content.
+ */
+
 public class HelpMain {
   private final String title;
   private final String body;
+
+  /**
+   * Constructs a new HelpMain item with the specified title and body content.
+   *
+   * @param title the title of the help item
+   * @param body  the body content of the help item
+   */
 
   public HelpMain(String title, String body) {
     this.title = title;
     this.body = body;
   }
 
+  /**
+   * Gets the title of the help item.
+   *
+   * @return the title of the help item
+   */
+
   public String getTitle() {
     return title;
   }
+
+  /**
+   * Gets the body content of the help item.
+   *
+   * @return the body content of the help item
+   */
 
   public String getBody() {
     return body;
